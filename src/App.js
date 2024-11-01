90b5bc2e416be054138142065180309392715aba
prernas29 <singhprerna455@gmail.com>
2024-11-01 11:43:07 +0530

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
-          {/* Protected routes */}
           <Route
             path="/"
             element={
